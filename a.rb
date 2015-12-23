3d5be34d982d61fc041eb8cf79b7705837780c04
Tomoyuki Sahara <sahara@caddr.net>
2015-12-23 22:55:16 +0900

    wip.

--- a.rb
+++ a.rb
@@ -2,15 +2,89 @@
 
 require 'openssl'
 
-rsa = OpenSSL::PKey::RSA.generate 2048
+$serial = 0
 
-cert = OpenSSL::X509::Certificate.new
-cert.version = 3
-cert.not_before = Time.now - 86400
-cert.not_after = Time.now + 86400
-cert.subject = OpenSSL::X509::Name.new([["CN", "Test Root CA"]])
-cert.public_key = rsa.public_key
+class CertUtils
+  def self.mkcert(h={})
+    h = {
+      :rsa_key_length => 2048,
+      :not_before => Time.now - 86400*30,
+      :not_after  => Time.now + 86400*30,
+      :hash_algorithm => :sha256
+    }.merge(h)
 
-cert.sign(rsa, OpenSSL::Digest::SHA1.new)
+    raise RuntimeError, ":cn is required (can be :none)" unless h[:cn]
 
-File.open("cert-root.pem", "w") { |f| f.write(cert.to_pem) }
+    unless h[:self_signed] || (h[:ca_certificate] && h[:ca_key])
+      raise RuntimeError, ":self_signed or :ca_certificate/:ca_key is required"
+    end
+
+    rsa = OpenSSL::PKey::RSA.generate h[:rsa_key_length]
+
+    cert = OpenSSL::X509::Certificate.new
+    cert.version = 3
+    cert.serial = ($serial += 1)
+    cert.not_before = h[:not_before]
+    cert.not_after = h[:not_after]
+    cert.subject = OpenSSL::X509::Name.new([[ "CN", h[:cn] ]])
+    cert.public_key = rsa.public_key
+
+    if h[:self_signed]
+      seq_true = OpenSSL::ASN1.Sequence([OpenSSL::ASN1::Boolean(true)])
+      ex = OpenSSL::X509::Extension.new('basicConstraints', seq_true)
+      cert.add_extension(ex)
+    end
+
+    if h[:self_signed]
+      issuer   = cert.subject
+      sign_key = rsa
+    else
+      issuer   = h[:ca_certificate].subject
+      sign_key = h[:ca_key]
+    end
+
+    hash = OpenSSL::Digest.new(h[:hash_algorithm].to_s)
+    cert.issuer = issuer
+    cert.sign(sign_key, hash)
+    [ cert, rsa ]
+  end
+end
+
+class Server
+  @@num = 0
+
+  def initialize
+    @no = (@@num += 1)
+    @dir = File.join(Dir.pwd, "site#{@no}")
+    begin
+      Dir.mkdir @dir
+    rescue Errno::EEXIST
+    end
+  end
+
+  def mkcert(h={})
+    cert, pkey = CertUtils.mkcert(h)
+    self.writefile "cert.pem", cert.to_pem
+    self.writefile "cert.der", cert.to_der
+    self.writefile "pkey.pem", pkey.to_pem
+  end
+
+  def writefile rpath, str
+    File.open(File.join(@dir, rpath), "w") { |f| f.write(str) }
+  end
+end
+
+root_cert, root_pkey = CertUtils.mkcert({
+  :cn => "100% Trustworthy Certificate Authority",
+  :self_signed => true,
+  :not_after => Time.now + 86400*365*10   # 10 years
+})
+File.open("root_cert.pem", "w") { |f| f.write(root_cert.to_pem) }
+
+s = Server.new
+s.mkcert({
+  :cn => "localhost",
+  :ca_certificate => root_cert,
+  :ca_key => root_pkey,
+  :not_after => Time.now + 86400*600
+})
